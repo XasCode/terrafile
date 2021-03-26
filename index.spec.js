6e0015f428b37665e7d4990b58d35012404161f6
Justin <JEStaubach@users.noreply.github.com>
2021-03-26 10:12:46 -0400

    add a sample cli test

---
+++ index.spec.js
@@ -0,0 +1,30 @@
+const path = require('path');
+const exec = require('child_process').exec;
+
+test('Code should be 0', async () => {
+  let result = await  cli(['run'], '.');
+  expect(result.code).toBe(0);
+});
+
+test('output should be ...', async () => {
+  let result = await  cli(['run'], '.');
+  expect(result.code).toBe(0);
+});
+
+
+function cli(args, cwd) {
+  return new Promise(resolve => { 
+    exec(
+      `node ${path.resolve('./index')} ${args.join(' ')}`,
+      { cwd },
+      (error, stdout, stderr) => {
+        resolve({
+          code: error && error.code ? error.code : 0,
+          error,
+          stdout,
+          stderr
+        });
+      }
+    );
+  });
+}
