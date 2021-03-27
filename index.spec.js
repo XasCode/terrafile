6314846da8cfcbd1f5d40841b330be661ebf21f0
Justin <JEStaubach@users.noreply.github.com>
2021-03-27 14:46:21 -0400

    Create table of test of the cli.

--- index.spec.js
+++ index.spec.js
@@ -1,26 +1,28 @@
 const path = require('path');
 const exec = require('child_process').exec;
-test(`Code should be 0`, async () => {
-  const result = await cli(['run'], '.');
-  expect(result.code).toBe(0);
+
+describe.each`
+  backend                    | args       | code    | expected
+  ${""}                      | ${"run"}   | ${0}    | ${`Hello World!\nThis is a test of requiring another file.\n`}
+  ${"./include.js"}          | ${"run"}   | ${0}    | ${`Hello World!\nThis is a test of requiring another file.\n`}
+  ${"./include.mock.js"}     | ${'run'}   | ${0}    | ${`Hello World!\n!!!\n`}
+`('$backend $args', ({backend, args, code, expected}) => {
+  test(`output should be ${expected}`, async () => {
+    const result = await cli(args.split(' '), '.', backend);
+    expect(result.stdout).toBe(expected);
+    expect(result.code).toBe(code);
+  });
+
 });
 
-test(`output should be ...`, async () => {
-  const result = await cli(['run'], '.');
-  expect(result.stdout).toBe(`Hello World!\nThis is a test of requiring another file.\n`);
-});
-
-test(`output should be !!!`, async () => {
-  const result = await cli(['run'], '.', `./include.mock.js`);
-  expect(result.stdout).toBe(`Hello World!\n!!!\n`);
-});
-
-
-function cli(args, cwd, api='./include.js') {
+function cli(args, cwd, api) {
   return new Promise(resolve => { 
     exec(
       `node ${path.resolve('./index')} ${args.join(' ')}`,
-      { env: { ...process.env, terrafile_be_api: api }, cwd },
+      {
+        env: api.length > 0 ? { ...process.env, terrafile_be_api: api } : { ...process.env },
+        cwd
+      },
       (error, stdout, stderr) => {
         resolve({
           code: error && error.code ? error.code : 0,
