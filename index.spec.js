653fcb785738abd5e7e8b0e778446d0e7186a965
Justin <JEStaubach@users.noreply.github.com>
2021-03-27 21:16:07 -0400

    reorganize tests into a single location

--- index.spec.js
+++ index.spec.js
@@ -1,12 +1,26 @@
 const path = require('path');
 const exec = require('child_process').exec;
 
+const consoleSpy = jest.spyOn(console, 'log').mockImplementation()
+
 describe.each`
   backend                    | args       | code    | expected
   ${""}                      | ${"run"}   | ${0}    | ${`Hello World!\nThis is a test of requiring another file.\n`}
   ${"./include.js"}          | ${"run"}   | ${0}    | ${`Hello World!\nThis is a test of requiring another file.\n`}
   ${"./include.mock.js"}     | ${'run'}   | ${0}    | ${`Hello World!\n!!!\n`}
 `('$backend $args', ({backend, args, code, expected}) => {
+
+  beforeEach(() => {
+    consoleSpy.mockClear()
+  });
+
+  test('included output should be ...', async () => {
+    const { printMsg } = require(backend.length > 0 ? backend : './include');
+    printMsg();
+    expect(console.log).toBeCalledTimes(1)
+    expect(console.log).toHaveBeenLastCalledWith(expected.split('\n')[1])
+  });
+
   test(`output should be ${expected}`, async () => {
     const result = await cli(args.split(' '), '.', backend);
     expect(result.stdout).toBe(expected);
