b68dc77d54881ff558d09ad63c0a248e15fc52c9
Justin <JEStaubach@users.noreply.github.com>
2021-03-29 23:48:52 -0400

    Remove hard coded test cases and generate test cases isntead. have wip to modularize CLI so that can run tests faster.

--- index.js
+++ index.js
@@ -1,21 +1,33 @@
 #!/usr/bin/env node
 
-const { Command, Option } = require('commander');
-const program = new Command();
-program
-  .version(require('./package.json').version, '-V, --version', 'Show version information for terrafile')
-  .description(`Manage vendored modules using a JSON file.`);
+function main(myargs, be) {
+  const { Command, Option } = require('commander');
 
-program
-  .command('install')
-  .description('Installs the files in your terrafile.json')
-  .action((options) => {
-    //console.log(`options: ${JSON.stringify(options)}`);
-    const backend = `${process.env.terrafile_be_api ? process.env.terrafile_be_api : './include'}`;
-    const { install } = require(backend);
-    install(options);
-  })
-  .addOption(new Option('-d, --directory <string>', 'module directory').default('vendor/modules'))
-  .addOption(new Option('-f, --file <string>', 'config file').default('terrafile.json'));
+  const program = new Command();
+  program
+    .version(require('./package.json').version, '-V, --version', 'Show version information for terrafile')
+    .description(`Manage vendored modules using a JSON file.`);
 
-program.parse(process.argv);
+  program
+    .command('install')
+    .description('Installs the files in your terrafile.json')
+    .action((options) => {
+      const backend = be == undefined ? `${process.env.terrafile_be_api ? process.env.terrafile_be_api : './include'}` : be;
+      const { install } = require(backend);
+      install(options);
+    })
+    .addOption(new Option('-d, --directory <string>', 'module directory').default('vendor/modules'))
+    .addOption(new Option('-f, --file <string>', 'config file').default('terrafile.json'));
+  
+  try {
+    program.parse(myargs);
+  } catch(err) {
+    console.log(JSON.stringify(err));
+  }
+}
+
+if (require.main === module) {
+  main(process.argv);
+}
+
+module.exports.main = main; 
