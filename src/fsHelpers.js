0058f3de6b60f0374bff35c1cf2b5c81aca76233
Justin <JEStaubach@users.noreply.github.com>
2021-04-22 01:42:45 -0400

    allow more time for rimraf to delete directories

--- src/fsHelpers.js
+++ src/fsHelpers.js
@@ -49,7 +49,7 @@ exports.touchFile = function (filePath, perms) {
 const rimrafDir = function (dir) {
   const absPath = getAbsolutePath(dir);
   if (absPath !== undefined && checkIfDirExists(dir)) {
-    rimraf(dir);
+    rimraf(dir, { maxBusyTries: 300 });
     return dir;
   } else {
     console.error(`Error deleting dir: ${dir}`);
