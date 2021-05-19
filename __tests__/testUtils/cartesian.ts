e16e9b253413d5316d9ad72324cda88b0cd775b9
Justin <JEStaubach@users.noreply.github.com>
2021-05-19 06:50:52 -0400

    function was used before defined

--- __tests__/testUtils/cartesian.ts
+++ __tests__/testUtils/cartesian.ts
@@ -1,16 +1,10 @@
-// cartesian() takes an array of arrays and otuputs a list of all combinations
-// made up of one element from each of the arrays.
-function cartesian(...args: unknown[][]): unknown[][] {
-  if (args.length === 0) return [];
-  return calcCartesian(...args);
-}
-
 // eslint-disable-next-line max-len
 // from: https://stackoverflow.com/questions/15298912/javascript-generating-combinations-from-n-arrays-with-m-elements
 function calcCartesian(...args: unknown[][]): unknown[][] {
   const r: unknown[][] = [];
   const max = args.length - 1;
   function helper(arr: unknown[], i: number) {
+    // eslint-disable-next-line no-plusplus
     for (let j = 0, l = args[i].length; j < l; j++) {
       const a: unknown[] = [...arr, args[i][j]];
       if (i === max) r.push(a);
@@ -19,6 +13,13 @@ function calcCartesian(...args: unknown[][]): unknown[][] {
   }
   helper([], 0);
   return r;
+}
+
+// cartesian() takes an array of arrays and otuputs a list of all combinations
+// made up of one element from each of the arrays.
+function cartesian(...args: unknown[][]): unknown[][] {
+  if (args.length === 0) return [];
+  return calcCartesian(...args);
 }
 
 export { cartesian };
