442297e331341010e83673813b8258e5613ce949
Justin <JEStaubach@users.noreply.github.com>
2021-06-05 09:32:33 -0400

    filter tests with network requests by default

---
+++ __tests__/testUtils/testFilter.ts
@@ -0,0 +1,24 @@
+type FilteredTest = {
+  message: string;
+  test: string;
+};
+
+type FilteredTests = {
+  filtered: FilteredTest[];
+};
+
+const ignoreStrings = ['backend.unmocked.spec.ts'];
+
+module.exports = async function filter(tests: string[]): Promise<FilteredTests> {
+  return Promise.resolve({
+    filtered: tests
+      .filter((test) => {
+        return ignoreStrings.reduce((acc, curr, i, arr) => {
+          return acc && test.indexOf(curr) === -1;
+        }, true);
+      })
+      .map((filterMatch) => {
+        return { message: 'skipping...', test: filterMatch };
+      }),
+  });
+};
