133f4d6fcf80ea932e4cc8200d7523ef7961deee
Justin <JEStaubach@users.noreply.github.com>
2021-06-15 20:42:06 -0400

    fix long line and remove implicit return

--- __tests__/revert.spec.ts
+++ __tests__/revert.spec.ts
@@ -23,7 +23,7 @@ describe(`test backend's ability to revert on error`, () => {
     rimrafDir(`vendor`);
   });
 
-  test(`1st install successfull, then 2nd install to same loc. should fail processing file and revert dir`, async () => {
+  test(`1st install successfull, then 2nd install to same loc should fail processing file and revert dir`, async () => {
     const configFile = `terrafile.sample.json`;
     const destination = `vendor/revert1`;
 
@@ -77,7 +77,9 @@ describe(`test backend's ability to revert on error`, () => {
       directory: destination,
       fetcher: fetcher.use(mockedFetcher.mock),
       cloner: cloner.use(mockedCloner.mockError),
-      createDir: (_: Path) => null,
+      createDir: (_: Path) => {
+        return null;
+      },
     });
 
     // verify expected directories exist; re-use testJson
