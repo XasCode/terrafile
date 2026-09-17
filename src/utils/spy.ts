15711f9e77d7c3b2adf4267684155597a48df12a
Justin <justin@jsdevtools.com>
2026-09-17 19:09:44 -0400

    fixed test suite error related to spy

--- src/utils/spy.ts
+++ src/utils/spy.ts
@@ -1,16 +1,14 @@
 import { vi } from 'vitest';
-import { populateGlobal } from 'vitest/environments';
 
 // setup(): spyOn/mock console messages, stderr, stdout, and process.exit
 function setup(): void {
-  const spyLog = vi.spyOn(console, `log`);
-  const spyErr = vi.spyOn(console, `error`);
-  const spyStdOut = vi.spyOn(process.stdout, `write`);
-  const spyStdErr = vi.spyOn(process.stderr, `write`);
-  const spyExit = vi.spyOn(process, `exit`).mockImplementation((): never => {
+  vi.spyOn(console, `log`);
+  vi.spyOn(console, `error`);
+  vi.spyOn(process.stdout, `write`);
+  vi.spyOn(process.stderr, `write`);
+  vi.spyOn(process, `exit`).mockImplementation((): never => {
     throw new Error(`exit`);
   });
-  populateGlobal(global, { spyLog, spyErr, spyStdOut, spyStdErr, spyExit });
 }
 
 // clear(): clean up mocked spy functions
